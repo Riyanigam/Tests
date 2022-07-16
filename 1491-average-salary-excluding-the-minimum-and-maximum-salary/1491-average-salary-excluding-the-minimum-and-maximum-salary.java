8639620f93d29ed3b9a9907dac615060a6c326a7
Riyanigam <42487198+Riyanigam@users.noreply.github.com>
2022-07-16 23:12:48 +0530

    Time: 1 ms (39.92%), Space: 41.6 MB (61.68%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -2,11 +2,12 @@ class Solution {
     public double average(int[] salary) {
         double s = 0, avg = 0;
         Arrays.sort(salary);
-        for(int i=1; i<salary.length-1; i++)
+        int n=salary.length;
+        for(int i=1; i<n-1; i++)
         {
-            s = s+salary[i];
+            s+=salary[i];
         }
-        avg = s/(salary.length-2);
+        avg = s/(n-2);
         return avg;
     }
 }
